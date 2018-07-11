3e6e00ef333e20bf62e0b323ca8f0beb3d35a84b
xiaoquanjie <404558965@qq.com>
2018-07-11 19:09:09 +0800

    add script

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
 
-root_path=`pwd`
-export LD_LIBRARY_PATH=$root_path/thirdparty_library/mysql/linux_lib64/:$root_path/thirdparty_library/protobuf/linux
+#source ./thirdparty_library/link.sh
+cp -r thirdparty_library/ ~/
+
+lib1=~/thirdparty_library/mysql/linux_lib64/
+lib2=~/thirdparty_library/protobuf/linux
+lib_path=$lib1:$lib2
+
+dst_file=~/.bash_profile
+
+grep 'export LIBRARY_PATH' $dst_file
+if [ $? -ne 0 ];then
+  echo adding.....
+  echo "export LIBRARY_PATH=$lib_path" >> $dst_file
+else
+  echo changing.....
+  sed -i "s%export LIBRARY_PATH=.*%export LIBRARY_PATH=$lib_path%g" $dst_file
+fi
+
+grep 'export LD_LIBRARY_PATH' $dst_file
+if [ $? -ne 0 ];then
+  echo adding.....
+  echo "export LD_LIBRARY_PATH=$lib_path" >> $dst_file
+else
+  echo changing.....
+  sed -i "s%export LD_LIBRARY_PATH=.*%export LD_LIBRARY_PATH=$lib_path%g" $dst_file
+fi
+
+
+source $dst_file
+
